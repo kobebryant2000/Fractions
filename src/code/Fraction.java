4f6c28612cb716d5d714d4561a0a5420438b7f0b
kobebryant2000 <sportsalex646@gmail.com>
2018-01-11 08:52:11 -0600

    Created fraction class with contructors and added a multiply method

---
+++ src/code/Fraction.java
@@ -0,0 +1,30 @@
+package code;
+
+public class Fraction {
+    private int num, denom;
+
+    public Fraction(int num, int denom) {
+        this.num = num;
+        this.denom = denom;
+    }
+
+    public Fraction(int num) {
+        this.num = num;
+        denom = 1;
+    }
+
+    public Fraction(Fraction other) {
+        num = other.num;
+        denom = other.denom;
+    }
+
+    public String toString() {
+        return num + "/" + denom;
+    }
+
+    public Fraction multiply(Fraction f)
+    {
+        return new Fraction (num*f.num, denom*f.denom);
+    }
+
+}
---
+++ src/code/Runner.java
@@ -0,0 +1,14 @@
+package code;
+
+public class Runner
+{
+    public static void main (String[] args)
+    {
+       Fraction myFraction = new Fraction(2,3 );
+       Fraction otherFraction = new Fraction(5);
+       Fraction a = myFraction;
+       Fraction multiplied = myFraction.multiply(otherFraction);
+       System.out.println(multiplied);
+
+    }
+}
